38232b0b4c4e5ad6af309f329552c51442fd33bb
Melissa Christman <melchrstmn@gmail.com>
2019-10-16 20:02:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,8 +10,9 @@ def sort_array_char_count(array)
   array.sort_by{ |word| word.length }
 end
 
-def swap_elements(array, index, destination_index)
-  array.sort {|a,b| a <=> b}
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
 def reverse_array(array)
